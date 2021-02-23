fef35d26ea7b5359a188488c602b3bac6dc5dae3
Ibrahim-Benkhedda <63601488+Ibrahim-Benkhedda@users.noreply.github.com>
2021-02-23 17:45:02 +0100

    Add files via upload

---
+++ master.js
@@ -0,0 +1,8 @@
+// NAV BAR
+const hamburger_menu = document.querySelector(".hamburger-menu");
+const container = document.querySelector(".container");
+
+hamburger_menu.addEventListener("click", () => {
+  container.classList.toggle("active");
+});
+// END OF NAV BAR
